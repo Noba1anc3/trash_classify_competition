0c3c8a9709a2a684cc20203ae104e9e335784b36
Noba1anc3 <zxryhjp@yahoo.co.jp>
2020-06-03 23:17:51 +0800

    change config

--- mmdetection/configs/_base_/datasets/voc0712.py
+++ mmdetection/configs/_base_/datasets/voc0712.py
@@ -1,11 +1,6 @@
 # dataset settings
 dataset_type = 'VOCDataset'
-classes = ('一次性快餐盒', '书籍纸张', '充电宝', '剩饭剩菜', '包', '垃圾桶', '塑料器皿', '塑料玩具',
-           '塑料衣架', '大骨头', '干电池', '快递纸袋', '插头电线', '旧衣服', '易拉罐', '枕头', '果皮果肉',
-           '毛绒玩具', '污损塑料', '污损用纸', '洗护用品', '烟蒂', '牙签', '玻璃器皿', '砧板', '筷子',
-           '纸盒纸箱', '花盆', '茶叶渣', '菜帮菜叶', '蛋壳', '调料瓶', '软膏', '过期药物', '酒瓶',
-           '金属厨具', '金属器皿', '金属食品罐', '锅', '陶瓷器皿', '鞋', '食用油桶', '饮料瓶', '鱼骨')
-data_root = 'data/VOCdevkit/'
+data_root = '/cache/datasets/VOCdevkit/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
@@ -39,7 +34,6 @@ data = dict(
     workers_per_gpu=16,
     train=dict(
         type='RepeatDataset',
-        classes=classes,
         times=1,
         dataset=dict(
             type=dataset_type,
@@ -52,14 +46,12 @@ data = dict(
     ),
     val=dict(
         type=dataset_type,
-        classes=classes,
         ann_file=data_root + 'VOC2007/ImageSets/Main/val.txt',
         img_prefix=data_root + 'VOC2007/',
         pipeline=test_pipeline
     ),
     test=dict(
         type=dataset_type,
-        classes=classes,
         ann_file=data_root + 'VOC2007/ImageSets/Main/val.txt',
         img_prefix=data_root + 'VOC2007/',
         pipeline=test_pipeline
